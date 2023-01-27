629976cc4cb46f7e6b894f053d330f76b478fc97
Christen <christen.bc@gmail.com>
2023-01-27 17:30:59 +0100

    implement state for every square so when clicked shows an x

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
-function Square({ value }: { value: string }) {
+function Square() {
+  const [value, setValue] = useState("");
+  // const [value, setValue] = useState<string | null>(null); Also this can be used
+
   function handleClick() {
-    console.log('clicked!');
+    setValue("X");
   }
+
   return <button className="square" onClick={handleClick}>{value}</button>;
 }
 
@@ -11,19 +16,19 @@ export default function Board() {
   return (
     <>
       <div className='board-row'>
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
