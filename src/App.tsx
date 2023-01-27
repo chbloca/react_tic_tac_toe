624a71d3aa7444300e1f2405876942aec67781b5
Christen <christen.bc@gmail.com>
2023-01-27 16:49:22 +0100

    add more squares

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import './App.css';
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
